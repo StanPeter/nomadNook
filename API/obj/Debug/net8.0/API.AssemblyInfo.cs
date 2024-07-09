8b29432a27dc0e3b37244e6d12bb1af1b1a6a584
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-09 12:39:01 +0200

    Changed for async, testing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b97d5016254de1e10652796d63134a115bbd5e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c4a175451762477da93f1e30ae1274693ab4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
