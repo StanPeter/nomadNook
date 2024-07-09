e0c4a175451762477da93f1e30ae1274693ab4e1
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-09 12:34:30 +0200

    Adding get user endpoint

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<User>> GetUsers()
+    {
+        var users = context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<User> GetUser(int id)
+    {
+        var user = context.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+}
