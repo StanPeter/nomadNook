054c993100a9da04e2b0608ad5b45ef7ddae36c1
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-10 21:34:56 +0200

    Adding client

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'client';
+  users: { userName: string; id: number }[] = [];
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: (data) => {
+        this.users = data as { userName: string; id: number }[];
+        console.log('User created', data);
+      },
+      error: (error) => {
+        console.error('Error creating user', error);
+      },
+      complete: () => {
+        console.log('User creation completed');
+      },
+    });
+  }
+}
