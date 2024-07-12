3da6b1a770a41de59928bb2cde5f89317051b33b
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-12 17:07:48 +0200

    Account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<User>> RegisterUser(RegisterDto registerDto)
+    {
+        using var hmac = new HMACSHA512();
+
+        var user = new User
+        {
+            UserName = registerDto.UserName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+}
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [MaxLength(50)]
+    [Required]
+    public required string UserName { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
