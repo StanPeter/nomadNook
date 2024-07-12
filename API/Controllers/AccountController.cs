b23de71381fb479ae6c79b29f64d6b3da20603ab
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-12 17:54:05 +0200

    Adding Login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,19 +4,22 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
-    [HttpPost]
-    public async Task<ActionResult<User>> RegisterUser(RegisterDto registerDto)
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(RegisterDto registerDto)
     {
+        if (await IsUserRegistered(registerDto.UserName)) return BadRequest("User already exists");
+
         using var hmac = new HMACSHA512();
 
         var user = new User
         {
-            UserName = registerDto.UserName,
+            UserName = registerDto.UserName.ToLower(),
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
@@ -25,5 +28,27 @@ public class AccountController(DataContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<User>> LoginUser(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName.ToLower());
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var hashedPassword = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < hashedPassword.Length; i++)
+        {
+            if (user.PasswordHash[i] != hashedPassword[i]) return Unauthorized("Passwords don't match");
+        }
+
+        return user;
+    }
+
+    private async Task<bool> IsUserRegistered(string userName)
+    {
+        return await context.Users.AnyAsync(u => u.UserName.ToLower() == userName.ToLower());
     }
 }
---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDto
+{
+    [MaxLength(50)]
+    [Required]
+    public required string UserName { get; set; }
+    [Required]
+    public required string Password { get; set; }
+}
