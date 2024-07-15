24cb8d93dd8a096f11b9ccb199140a8933774d41
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-15 14:44:08 +0200

    adding register form

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatToolbar, MatInputModule, CarouselModule],
+  imports: [MatToolbar, MatInputModule, CarouselModule, RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
