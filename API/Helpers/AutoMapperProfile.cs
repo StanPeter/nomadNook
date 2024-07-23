44c6252773c4382dd1eb0c3b7c09808cfe65c5a6
Stanislav Petr <stanislav.petr7@gmail.com>
2024-07-23 19:40:43 +0200

    Automapper for users

---
+++ API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<User, MemberDto>();
+        CreateMap<Photo, PhotoDto>();
+    }
+}
